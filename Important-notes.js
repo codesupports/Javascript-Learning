ade6ed4e17e5fe79c83074ee7897bd4f5d4872dd
Raj Kumar Maheshwari <85394585+codesupports@users.noreply.github.com>
2024-09-10 14:54:22 +0530

    Create Important-notes.js

---
+++ Important-notes.js
@@ -0,0 +1,6 @@
+Math.floor() : 
+  Purpose: Rounds a number down to the nearest integer.
+
+Math.random():
+  Purpose: Generates a pseudo-random floating-point number between 0 (inclusive) and 1 (exclusive).
+  It takes no parameters and returns a number in the range [0, 1).
