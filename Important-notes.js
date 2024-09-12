d844aed91952ab85688df85815514ef6fe467ba7
Raj Kumar Maheshwari <85394585+codesupports@users.noreply.github.com>
2024-09-12 16:56:29 +0530

    pdate Important-notes.jsU

--- Important-notes.js
+++ Important-notes.js
@@ -4,3 +4,7 @@ Math.floor() :
 Math.random():
   Purpose: Generates a pseudo-random floating-point number between 0 (inclusive) and 1 (exclusive).
   It takes no parameters and returns a number in the range [0, 1).
+React PureComponent: 
+  preventing unnecessary re-renders
+  Avoids Unnecessary Re-renders
+  In functional components, you can use React.memo, which serves a similar purpose to PureComponent for optimizing performance by memoizing the component.
