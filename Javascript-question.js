eef08ce800d68b0393468eb3aad6dd444f715bfb
Raj Kumar Maheshwari <85394585+codesupports@users.noreply.github.com>
2024-09-22 12:22:53 +0530

    Update Javascript-question.js

--- Javascript-question.js
+++ Javascript-question.js
@@ -382,7 +382,7 @@ console.log(isPrime(13)) // output: 13 is a Prime number
 const arr = [2,3,2,3,5]
 let sum =0;
 for(let i=0; i <arr.length; i++){
-  sum = sum + arr[i]
+  sum = sum + arr[i]  or // sum += arr[i]
 }
 console.log(sum) // 15
 
