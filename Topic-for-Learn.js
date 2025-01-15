5cf8b21ff91983bbcf24f9fd21712b5ebcbee58f
Raj Kumar Maheshwari <85394585+codesupports@users.noreply.github.com>
2025-01-15 12:33:32 +0530

    Create Topic-for-Learn.js

---
+++ Topic-for-Learn.js
@@ -0,0 +1,66 @@
+HTML - 5 
+	webworkers
+	HTML 5 new features
+	New input features
+	Web API (Geolocation, session storage, localStorage)
+	data-* attributes
+	Website optimization tricks 
+	SVG sprite and how to use and write code
+	Image optimization how to optimize
+	Type of image usage (jpg, png, webp)
+	Explain the webP images
+	Picture tag and how to write code
+	Grid all property 
+	what would be the effect if we are using more class name in html. It would be effect page optimization?
+	In bootstap we write the custom css.
+	BoxModel
+	Box-shadow
+	Sementic Tag - Explain 
+	Difference between grid and Flex
+	Sticky position will work all the browser or not
+	Explain the all Positions
+	
+Git Version - 
+	How to merge file conflict
+
+ReactJS-
+	How to optimize react application
+	What design pattern should use in react
+	Context API integration
+	What is virtual Dom
+	Hooks
+	HOC
+	Memoization
+	Optimization
+	Difference between useState and useref()
+	CustomHooks 
+	In custom hooks we can use JSX and its necessary return something
+	with parent component have one button and click then we can child component input focus
+	We can useRef if there is no dom element in component.
+	
+	
+JavaScript -
+	Design Pattern
+	Debouncing
+	Throatling
+	Hoisting
+	Closure (Advantage and disadvantgae, realtime example)
+	CallBack
+	EventLoop
+	Scopping
+	Let, Var, const
+	curring
+	event delegation
+	datatype
+	event bubbling and capturing
+	callback
+	Promise - promise.all, promise.allSettled, promise.race, promise.any
+	async/await
+	ES6 new features
+	all,apply,bind
+	
+
+TypeScript - 
+	
+	
+Node JS - 
